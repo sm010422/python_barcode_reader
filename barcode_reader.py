3dae9c6059f27d97f74f52afcb629f0a9e57e9b7
ParkSangMin <sm010422@naver.com>
2023-12-23 22:51:44 +0900

    python_barcode_reader_v2

--- barcode_reader.py
+++ barcode_reader.py
@@ -1,14 +1,30 @@
 import cv2
 from pyzbar.pyzbar import decode
+#from pydub import AudioSegment 
+#from pydub.playback import play 
 
 
 # capture webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
+#song =  AudioSegment.from_wav("Barcode-scanner-beep-sound.wav")
 while cap.isOpened():
     success, frame = cap.read()
 
     frame = cv2.flip(frame, 1)
+
+    detectedBarcode = decode(frame)
+    if not detectedBarcode:
+        print("No Barcode")
+    else:
+        for barcode in detectedBarcode:
+            if barcode.data != "":
+                cv2.putText(frame,str(barcode.data),(50,50),cv2.FONT_HERSHEY_COMPLEX,2,(0,255,255),2)
+                #play(song)
+                cv2.imwrite("code.png",frame)
+                
+
     cv2.imshow('scanner', frame)
     if cv2.waitKey(1) == ord('q'):
         break
+
